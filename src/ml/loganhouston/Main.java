440ad7f5c091cd1c011fdf60c3cbe782c6bf259f
Logan Houston <logan@loganhouston.ml>
2019-07-02 02:57:34 -0500

    Fixed some stuff

--- src/ml/loganhouston/Main.java
+++ src/ml/loganhouston/Main.java
@@ -1,20 +1,37 @@
 package ml.loganhouston;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+
 public class Main extends JavaPlugin implements Listener {
 
-    private String bean = "I'm a string bean!";
+    public static Main plugin;
+
+
 
     @Override
     public void onEnable() {
-        System.out.println("Farmer Perk Enabled!");
-
+        getServer().getPluginManager().registerEvents(this, this);
+        Bukkit.broadcastMessage("Farmer Perk starting!");
+        Bukkit.broadcastMessage("88                                             \n" +
+                "88                                             \n" +
+                "88                                             \n" +
+                "88,dPPYba,   ,adPPYba, ,adPPYYba, 8b,dPPYba,   \n" +
+                "88P'    \"8a a8P_____88 \"\"     `Y8 88P'   `\"8a  \n" +
+                "88       d8 8PP\"\"\"\"\"\"\" ,adPPPPP88 88       88  \n" +
+                "88b,   ,a8\" \"8b,   ,aa 88,    ,88 88       88  \n" +
+                "8Y\"Ybbd8\"'   `\"Ybbd8\"' `\"8bbdP\"Y8 88       88\n");
+        plugin = this;
     }
 
     @Override
     public void onDisable() {
-        System.out.println("Farmer Perk Disabled!");
+        Bukkit.broadcastMessage("Shutting down...");
+    }
+
+    public static Main getPlugin() {
+        return plugin;
     }
 }
